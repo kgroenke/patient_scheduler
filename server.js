ca9d41a95669330902f76411b1729ba6531b5f3b
Kelsey Groenke <krgroenke@gmail.com>
2015-12-17 14:09:59 -0800

    found body-parser error

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var app = express ();
 var path = require("path");
 
-// var bodyParser = require("body-Parser");
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "./client/static")));
 
