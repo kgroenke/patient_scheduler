6acb61463772851fbc50a505aa2839af0f653ba2
Kelsey Groenke <krgroenke@gmail.com>
2016-01-02 17:23:25 -0800

    added mongolab connection

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,21 @@ var mongoose = require('mongoose');
 var path = require('path');
 var fs = require('fs');
 
-mongoose.connect('mongodb://localhost/new_black_belt_exam');
+var uristring =
+   process.env.MONGOLAB_URI ||
+   process.env.MONGOHQ_URL ||
+   'mongodb://localhost/patient_scheduler';
+
+// var theport = process.env.PORT || 5000;
+mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
+
+// mongoose.connect('mongodb://localhost/new_black_belt_exam');
 
 var models_path = path.join(__dirname, './../models');
 
